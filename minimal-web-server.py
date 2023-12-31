cfc58e96bc274fa3b3764e5a8e351852a3cb932b
Fynnwind <64282552+Fynnwind@users.noreply.github.com>
2023-12-31 19:23:45 +0100

    switch string for html page

--- minimal-web-server.py
+++ minimal-web-server.py
@@ -3,32 +3,43 @@ import socket
 HOST, PORT = '', 8090
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # create standard socket (OS provides standard communication endpoint)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # manipulate socket at api level to prevent an error
-                                                                        # the SO_REUSEADDR flag tells the kernel to reuse a
-                                                                        # local socket in TIME_WAIT state, without waiting for
-                                                                        # its natural timeout to expire
-listen_socket.bind((HOST, PORT))    # When a socket is created with socket(2), it exists in a name space (address family) but has no
-                                    # address assigned to it.  bind() assigns the address specified by addr to the socket
-listen_socket.listen(1)     # Enable a server to accept connections it specifies the number of unaccepted connections that the system
-                            # will allow before refusing new connections
+listen_socket.bind((HOST, PORT))                                        # bind assigns ip adress and port to the socket
+listen_socket.listen(1)                                                 # Enable socket to accept incoming connections (backlog size 1)
+
 print(f'Serving HTTP on port {PORT} ...')
 while True:
-    client_connection, client_address = listen_socket.accept()  # Accept a connection. The socket must be bound to an address and
-                                                                # listening for connections. The return value is a pair (conn, address)
-                                                                # where conn is a new socket object usable to send and receive data on
-                                                                # the connection, and address is the address bound to the socket on the
-                                                                # other end of the connection.
-    request_data = client_connection.recv(1024)     # Receive data from the socket. The return value is a bytes object representing the
-                                                    # data received. The maximum amount of data to be received at once is specified by bufsize.
-    print(request_data.decode('utf-8')) # parse HTTP header
+    client_connection, client_address = listen_socket.accept()  # wait till client connects (opens a new soccet client_connection)
+    request_data = client_connection.recv(1024)                 # get received data and specify buffer size
+    print(request_data.decode('utf-8'))                         # parse HTTP header
 
-    http_response = b"""\
-HTTP/1.1 200 OK
-
-Hello, World!
-"""
-    client_connection.sendall(http_response)    # Send data to the socket. The socket must be connected to a remote socket.
-#   client_connection.close()                   # Close connection
-
-# To connect to the webserver use 'telnet localhost 8090' then type the get http request 'GET HTTP/1.1'
-# Alternitively go to your webbrowser and type the URL 'localhost:8080' 
+    # Hardcoded HTML content
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Simple HTML Page</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+        <p>This is a simple hardcoded HTML page sent over a socket.</p>
+    </body>
+    </html>
+    """
+    
+    # HTTP response headers
+    response_headers = {
+        "Content-Type": "text/html",
+        "Content-Length": str(len(html_content))
+    }
+    
+    # Construct the HTTP response
+    response = "HTTP/1.1 200 OK\r\n"
+    for header, value in response_headers.items():
+        response += f"{header}: {value}\r\n"
+    response += "\r\n"  # End of headers
+    response += html_content
+    
+    # Send the HTTP response over the socket using sendall
+    client_connection.sendall(response.encode('utf-8'))
+    client_connection.close()
